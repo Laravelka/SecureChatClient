160caa2fc7bd0289143215444c5008f0e1f255e3
Laravelka <joker777237@gmail.com>
2022-07-06 05:00:28 +0300

    cheto dobavil x2

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,9 +1,15 @@
 <template>
 	<ion-page class="video-bg h-100 ion-justify-content-center ion-align-items-center">
+		<video playsinline autoplay muted loop poster="cake.jpg">
+			<source
+				src="https://node.gameplace.space/files/bg-1.mp4"
+				type="video/mp4">
+			Your browser does not support the video tag.
+		</video>
 		<div class="text-dark ion-padding ion-margin">
 			<b>SAC</b> - The Secure Anonymous Chat
 		</div>
-		<ion-button color="success">Start</ion-button>
+		<ion-button color="success" round>Start</ion-button>
 	</ion-page>
 </template>
 <script lang="ts">
@@ -25,6 +31,26 @@
 	});
 </script>
 <style lang="css">
+	video {
+		object-fit: cover;
+		position: absolute;
+		top: 0;
+		left: 0;
+		height: 100%;
+		width: 100%;
+	}
+
+	.video-wrapper {
+		border: 2px solid #000;
+		width: 300px;
+		height: 300px;
+		position: relative;
+		overflow: hidden;
+		text-align: center;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 
 	.video-bg {
 		background-image: url(https://web.telegram.org/stream/%7B%22dcId%22%3A2%2C%22location%22%3A%7B%22_%22%3A%22inputDocumentFileLocation%22%2C%22id%22%3A%225330040721980268146%22%2C%22access_hash%22%3A%227434288464623762757%22%2C%22file_reference%22%3A%5B2%2C79%2C0%2C21%2C202%2C0%2C0%2C0%2C228%2C98%2C196%2C233%2C133%2C229%2C40%2C22%2C196%2C168%2C48%2C9%2C238%2C176%2C218%2C74%2C49%2C77%2C221%2C147%2C217%5D%7D%2C%22size%22%3A7619001%2C%22mimeType%22%3A%22video%2Fmp4%22%2C%22fileName%22%3A%22clouds_1.mp4%22%7D);
